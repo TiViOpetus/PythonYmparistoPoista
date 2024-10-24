b597ee0fba1f5bbfb73c8298d0cda5501b2dd865
Mika Vainio <koti.mika.vainio@gmail.com>
2024-10-24 14:59:42 +0300

    app torstain 24.10.2024 tilanne

--- app.py
+++ app.py
@@ -15,22 +15,40 @@ import identityCheck2
 # ---------
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
-# TODO: Pääohjelman ikuinen silmukka, josta poistutaan tarvittaessa (keksi mekanismi itse)
-# TODO: Paranna pääohjelmaa siten, että se ei kaadu, kun käyttäjä syöttää virheellisen henkilötunnuksen
-userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
-userGivenLastname = input('Syötä asiakkan sukunimi')
-# TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
-userGivenFirstname = input('Syötä asiakkan etunimi')
-# TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
-# TODO: Varaudu tilanteeseen, jossa hetu:n tarkiste on annettu pienillä kirjaimilla
-# TODO: Muuta syötettyjen nimien alkukirjain isoksi
 
-ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
-if ssnToCheck.isValidSsn() == True:
-    ssnToCheck.getDateOfBirth()
-    ssnToCheck.getGender()
-    age = ssnToCheck.calculateAge()
-    print('Syntymäaika:', ssnToCheck.dateOfBirth)
-    print('Ikä:', age)
-    print('Sukupuoli:', ssnToCheck.gender)
+while True:
+    
+    userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
+    userGivenSsn = userGivenSsn.upper() # Varmistetaan, että tarkiste on isolla
+    
+    # TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
+
+    # TODO: Rakenna funktio, jolla kysytään nimet ja muutetaan yhdysnimet isoille alkukirjaimille -> reg exp
+    
+    
+    
+
+    ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
+    if ssnToCheck.isValidSsn() == True:
+        try:
+            ssnToCheck.getDateOfBirth()
+            ssnToCheck.getGender()
+            age = ssnToCheck.calculateAge()
+            userGivenLastname = input('Syötä asiakkaan sukunimi: ')
+            userGivenLastname = userGivenLastname.capitalize()
+            userGivenFirstname = input('Syötä asiakkaan etunimi: ')
+            userGivenFirstname = userGivenFirstname.capitalize()
+            print('Asiakas:', userGivenLastname, userGivenFirstname)
+            print('Syntymäaika:', ssnToCheck.dateOfBirth)
+            print('Ikä:', age)
+            print('Sukupuoli:', ssnToCheck.gender)
+        except Exception as e:
+            print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe', e)
+        
+
+    # Kysytään halutaanko poistua ohjelmasta
+    wantAbort = input('Haluatko päättää ohjelman k/E: ')
+     # Muutetaan vastaus isoiksi kirjaimiksi ja tarkistetaan onko vastaus K
+    if wantAbort.upper() == 'K':
+        break # Poistutaan ikuisesta silmukasta
 
