b3754fd843526bdaf82fec6ae064492d0882d7e3
Mika Vainio <koti.mika.vainio@gmail.com>
2024-10-25 13:46:50 +0300

    Perjantain 25.10 päätös

--- app.py
+++ app.py
@@ -15,40 +15,77 @@ import identityCheck2
 # ---------
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
+# FUNKTIOT
+# --------
 
-while True:
-    
-    userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
-    userGivenSsn = userGivenSsn.upper() # Varmistetaan, että tarkiste on isolla
-    
-    # TODO: Tee tarkistus siitä, että nimi ei voi olla tyhjä
+def askName(question: str) -> str:
+    """Promts user to answer a question and converts the answer to title and removes white space
 
-    # TODO: Rakenna funktio, jolla kysytään nimet ja muutetaan yhdysnimet isoille alkukirjaimille -> reg exp
-    
-    
-    
+    Args:
+        question (str): Prompt to user
 
-    ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
-    if ssnToCheck.isValidSsn() == True:
-        try:
-            ssnToCheck.getDateOfBirth()
-            ssnToCheck.getGender()
-            age = ssnToCheck.calculateAge()
-            userGivenLastname = input('Syötä asiakkaan sukunimi: ')
-            userGivenLastname = userGivenLastname.capitalize()
-            userGivenFirstname = input('Syötä asiakkaan etunimi: ')
-            userGivenFirstname = userGivenFirstname.capitalize()
-            print('Asiakas:', userGivenLastname, userGivenFirstname)
-            print('Syntymäaika:', ssnToCheck.dateOfBirth)
-            print('Ikä:', age)
-            print('Sukupuoli:', ssnToCheck.gender)
-        except Exception as e:
-            print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe', e)
+    Returns:
+        str: modifies answer
+    """
+    name = ''
+    while name == '':
+        question = question + ': '
+        name = input(question).strip()
+    name = name.title()
+    return name
+
+
+
+if __name__ == "__main__":
+
+    # PÄÄOHJELMAN IKUINEN SILMUKKA
+    # ============================
+    while True:
+
+        # Alustetaan muuttujat tyhjiksi
+        userGivenSsn = ''
+        userGivenLastname = ''
+        userGivenFirstname = ''
         
+        # Kysytään asiaakkan henkilötunnus ja muutetaan kirjaimet isoiksi
+        userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
+        userGivenSsn = userGivenSsn.upper() # Varmistetaan, että tarkiste on isolla
 
-    # Kysytään halutaanko poistua ohjelmasta
-    wantAbort = input('Haluatko päättää ohjelman k/E: ')
-     # Muutetaan vastaus isoiksi kirjaimiksi ja tarkistetaan onko vastaus K
-    if wantAbort.upper() == 'K':
-        break # Poistutaan ikuisesta silmukasta
+        
+        
+        
+        
+        # Luodaan syötetystä henkilötunnuksesta NationalSSN-objekti
+        ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
 
+        # Tarkistetaan onko henkilötunnus oikein muodostettu
+        if ssnToCheck.isValidSsn() == True:
+
+            # Virheenkäsittely, mahdollisen vuosisatakoodivirheen varalta 
+            try:
+                ssnToCheck.getDateOfBirth() # Asetetaan syntymäaika ominaisuus
+                ssnToCheck.getGender() # Asetetaan sukupuoliominaisuus
+                age = ssnToCheck.calculateAge() # Lasketaan ikä tänään
+
+                # Kysytään loput tiedot, jos ei virhettä
+                userGivenLastname = askName('Asiakkaan sukunimi') 
+                userGivenFirstname = askName('Asiakkaan etunimi')
+
+                # Tulostetaan tiedot ruudulle
+                print('Asiakas:', userGivenLastname, userGivenFirstname)
+                print('Syntymäaika:', ssnToCheck.dateOfBirth)
+                print('Ikä:', age)
+                print('Sukupuoli:', ssnToCheck.gender)
+
+            # Virhetilanteessa näytetään virheilmoitus
+            except Exception as e:
+                print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe', e)
+
+        else:
+            print('Henkilötunnuksesa virhe, syötä tunnus uudelleen')  
+
+        # Kysytään halutaanko poistua ohjelmasta
+        wantToAbort = input('Haluatko päättää ohjelman k/E: ')
+        # Muutetaan vastaus isoiksi kirjaimiksi ja tarkistetaan onko vastaus K
+        if wantToAbort.upper() == 'K':
+            break # Poistutaan ikuisesta silmukasta
